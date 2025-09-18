50d71c8d39b3b801057bab3837fe902e19b97fd1
Mirzakhmet Syzdykov <mirzakhmets@icloud.com>
2025-09-18 17:53:18 +0500

    Signature and install project

---
+++ sign.bat
@@ -0,0 +1,3 @@
+signtool sign /f "G:\my\Security\Signature\OVG-Developers.pfx" /p "_vbpl9521" /fd sha256 /tr http://timestamp.digicert.com /td sha256 "C:\Users\mirzakhmets\Documents\GitHub\MedicalEq\MedicalEq\bin\Release\MedicalEq.exe"
+
+signtool sign /f "G:\my\Security\Signature\OVG-Developers.pfx" /p "_vbpl9521" /fd sha256 /tr http://timestamp.digicert.com /td sha256 "Documents\MedicalEq.exe"
